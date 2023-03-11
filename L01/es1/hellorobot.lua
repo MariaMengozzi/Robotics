ad4d0ccfd55a4354f3dc8be05a2793dca2949db8
Maria <mengozzi.maria@gmail.com>
2023-03-11 08:35:59 +0100

    doc: add documentation

--- L01/es1/hellorobot.lua
+++ L01/es1/hellorobot.lua
@@ -1,7 +1,7 @@
 -- Put your global variables here
 
 MOVE_STEPS = 15
-MAX_VELOCITY = 15
+MAX_VELOCITY = 20
 LIGHT_THRESHOLD = 1.5
 
 n_steps = 0
@@ -17,40 +17,37 @@ function init()
 end
 
 
-
---[[ This function is executed at each time step
-     It must contain the logic of your controller ]]
 function step()
 	n_steps = n_steps + 1
 
 	if (robot.light[1].value < robot.light[21].value) or (robot.light[12].value < robot.light[18].value) then
+		-- turn right
 		left_v = MAX_VELOCITY
 		right_v = 0
 		robot.wheels.set_velocity(left_v, right_v)
 	elseif (robot.light[1].value < robot.light[3].value) or (robot.light[12].value < robot.light[9].value) then
+		-- turn left
 		left_v = 0
 		right_v = MAX_VELOCITY
 		robot.wheels.set_velocity(left_v, right_v)
 	elseif (robot.light[1].value < robot.light[12].value) then
+		-- rotate
 		left_v = -MAX_VELOCITY
-		right_v = -MAX_VELOCITY
+		right_v = MAX_VELOCITY
 		robot.wheels.set_velocity(left_v, right_v)
 	elseif (robot.light[1].value > robot.light[3].value) then
+		-- proceeds in that direction
 		left_v = MAX_VELOCITY
 		right_v = MAX_VELOCITY
 		robot.wheels.set_velocity(left_v, right_v)
 	elseif (robot.light[1].value == 0) or (robot.light[12].value == 0) then
+		-- moves randomically
 		left_v = robot.random.uniform(0,MAX_VELOCITY)
 		right_v = robot.random.uniform(0,MAX_VELOCITY)
 		robot.wheels.set_velocity(left_v, right_v)
 	end
 end
 
---[[ This function is executed every time you press the 'reset'
-     button in the GUI. It is supposed to restore the state
-     of the controller to whatever it was right after init() was
-     called. The state of sensors and actuators is reset
-     automatically by ARGoS. ]]
 function reset()
 	left_v = robot.random.uniform(0,MAX_VELOCITY)
 	right_v = robot.random.uniform(0,MAX_VELOCITY)
@@ -59,9 +56,6 @@ function reset()
 end
 
 
-
---[[ This function is executed only once, when the robot is removed
-     from the simulation ]]
 function destroy()
    -- put your code here
 end
