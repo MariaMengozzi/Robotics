05fa352fa8278cbd0fb703c8db89037cdadd5256
Maria <mengozzi.maria@gmail.com>
2023-04-01 16:30:55 +0200

    fix: correct behavior and add description

--- L04/hellorobot.lua
+++ L04/hellorobot.lua
@@ -4,11 +4,11 @@ local vector = require "vector"
 
 MOVE_STEPS = 15
 MAX_VELOCITY = 15
-VFACTOR = 5
+
 PROX_THRESHOLD = 0.2
 LIGHT_THRESHOLD = 0.01 -- it should be set to a value higher than noise
 RANDOM_WALK_THRESHOLD = 0.01
-n_steps = -1
+n_steps = 0
 L = robot.wheels.axis_length
 
 
@@ -42,58 +42,70 @@ function random_walk()
 	valueOA, angleOA, idxOA = findmax(robot.proximity)
 	valueP, angleP, idxP = findmax(robot.light)
 
-	if (valueOA <= RANDOM_WALK_THRESHOLD and valueP <= RANDOM_WALK_THRESHOLD) then
-		return {length = 1, angle = robot.random.uniform(0, math.pi)}
-	else
-		return {length = 0, angle = 0}
-	end
+	 -- Motor schema
+    if (valueP < RANDOM_WALK_THRESHOLD) and (valueOA < RANDOM_WALK_THRESHOLD) then
+        -- move random
+        if n_steps % MOVE_STEPS == 0 then
+            randomAngle = robot.random.uniform(-math.pi, math.pi)
+            n_steps = 0
+        end
+        n_steps = n_steps + 1
+        return {length = 1, angle = randomAngle}
+    end
+    
+    return {length = 0.0, angle = 0.0}
 
 end
 
 
 
 function collision_avoidance()
-	value, angle, idx = findmax(robot.proximity)
-	if angle < 0 then 
-		return {length = value, angle = math.pi + angle} --repulsive force
-	else 
-		return {length = value, angle = angle - math.pi}
-	end
+	value, proximityAngle, idx = findmax(robot.proximity)
+	return { length = value, angle = (-(proximityAngle/proximityAngle)*math.pi + proximityAngle)  }
 end
 
 
 function photo_taxis()
-	value, angle, idx = findmax(robot.light)
-	return {length = 1 - value, angle = angle} --actractive force
+	value, lightAngle, idx = findmax(robot.light)
+	return {length = 1 - value, angle = lightAngle} --actractive force
 end
 
+
+function toDifferential(v)
+    return {
+        left = 1 * v.length * MAX_VELOCITY - L / 2 * v.angle,
+        right = 1 * v.length * MAX_VELOCITY + L / 2 * v.angle
+    }
+end
 
 --[[ This function is executed every time you press the 'execute'
      button ]]
 function init()
-  n_steps = -1
-	left_v = robot.random.uniform(0,MAX_VELOCITY)
-	right_v = robot.random.uniform(0,MAX_VELOCITY)
-	robot.wheels.set_velocity(left_v,right_v)
+
 end
+
 
 
 --[[ This function is executed at each time step
      It must contain the logic of your controller ]]
 function step()
-	n_steps = n_steps + 1
-	randomV = random_walk()
-	pV = photo_taxis()
-	oaV = collision_avoidance()
 
-	sumV = vector.vec2_polar_sum(randomV, pV)
-	sumV = vector.vec2_polar_sum(sumV, oaV)
+	behaviors = {
+        random_walk(),
+		photo_taxis(),
+        collision_avoidance()
+    }
 
-	left_v = limit_velocity((MAX_VELOCITY * 1 * sumV.length) - (sumV.angle * L/2))
-	right_v = limit_velocity((MAX_VELOCITY * 1 * sumV.length) + (sumV.angle * L/2))
+	sumV = {length = 0.0, angle = 0.0}
 
-	
-	robot.wheels.set_velocity(left_v, right_v)
+	for i=1,#behaviors do
+        sumV = vector.vec2_polar_sum(sumV, behaviors[i])
+    end
+
+	-- to differential
+	velocities = toDifferential(sumV)
+
+	robot.wheels.set_velocity(limit_velocity(velocities.left), limit_velocity(velocities.right))
 
 end
 
